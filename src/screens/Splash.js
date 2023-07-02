572933e01b3a5372c2ef44bd1f292625bb2cfbdc
Myda <momalafzal20@gmail.com>
2023-07-02 22:01:47 +0500

    :fire: Project Setup

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,12 +1,12 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 
-const App = () => {
+const Splash = () => {
   return (
     <View>
-      <Text>App</Text>
+      <Text>Splash</Text>
     </View>
   );
 };
 
-export default App;
+export default Splash;
